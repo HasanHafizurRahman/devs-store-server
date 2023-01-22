fe1485a175ccce40d3b92092c27f4e7f6bb648d0
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-22 21:29:27 +0600

    vercel config added

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_TevYUTGEBWal7EejDPxNKAxIRjxC/2MbqcedWLU",
+      apiToken: "LcQWrW1BU6JZChDD7v9tWJhi",
+      appFilter: "devs-store-server",
+      teamFilter: "team_S6Hwc4y69K7AdkFwc0JJLy1H",
+      roles: [],
+    },
+  },
+});
--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
+
+// vercel token : LcQWrW1BU6JZChDD7v9tWJhi
+// project id : prj_TevYUTGEBWal7EejDPxNKAxIRjxC
+// deployhook: "https://api.vercel.com/v1/integrations/deploy/prj_TevYUTGEBWal7EejDPxNKAxIRjxC/2MbqcedWLU"
+//  roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
